7515015f4b8c720eda2cb029f977aa5dc391ddd9
Saurav Sah <sah_64@yahoo.com>
2022-01-17 17:58:43 -0500

    added passport local strategy

---
+++ server/models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+	first_name: { type: String, required: true },
+	last_name: { type: String, required: true },
+	email: { type: String, required: true },
+	password: { type: String, required: true },
+	profile_picture: { type: Buffer, contentType: String },
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
---
+++ server/passport-config.js
@@ -0,0 +1,20 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcrpytjs");
+const User = require("./models/user");
+
+passport.use(
+	new LocalStrategy((username, password, done) => {
+		User.findOne({ email: username }, (err, user) => {
+			if (err) return done(err);
+			if (!user) return done(null, false, { message: "Incorrect email" });
+			bcrypt.compare(password, user.password, (err, res) => {
+				if (res) {
+					return done(null, user);
+				} else {
+					return done(null, false, { message: "Incorrect password" });
+				}
+			});
+		});
+	})
+);
