648d6f3f27432045553b1a762befbac94ba53c9e
Saurav Sah <sah_64@yahoo.com>
2022-01-29 21:08:39 -0500

    finished setup of login and signup routes with local auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<Login />} />
+				<Route exact path="/login" element={<Login />} />
+				<Route exact path="/signup" element={<Signup />} />
 			</Routes>
 		</BrowserRouter>
 	);
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,19 +1,78 @@
 import { React, useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import "./Login.css";
 
 function Login() {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [errorMessage, setErrorMessage] = useState("");
+
+	const changeEmail = (e) => {
+		setEmail(e.target.value);
+	};
+
+	const changePassword = (e) => {
+		setPassword(e.target.value);
+	};
+
+	const authenticate = async (e) => {
+		e.preventDefault();
+		try {
+			const response = await fetch("http://localhost:5000/api/login", {
+				method: "POST",
+				mode: "cors",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({ email, password }),
+			});
+
+			const userData = await response.json();
+			if (!userData.user) {
+				setErrorMessage(userData.info);
+			} else {
+			}
+		} catch (err) {
+			alert(err);
+		}
+	};
+
+	useEffect(() => {
+		console.log(errorMessage);
+	}, [errorMessage]);
+
 	return (
 		<div className="login-container">
 			<div className="login-container__details">
 				<h1 className="main-header">Agora</h1>
-				<h2 className="secondary-header">Login</h2>
-				<form className="login-form">
-					<input type="text" name="email" placeholder="Email" />
-					<input type="text" name="password" placeholder="Password" />
-					<button className="btn" type="submit">
+				<h2 className="secondary-header">Log In</h2>
+				<form className="login-form" action="">
+					<input
+						type="text"
+						name="email"
+						placeholder="Email"
+						value={email}
+						onChange={changeEmail}
+						required
+					/>
+					<input
+						type="password"
+						name="password"
+						placeholder="Password"
+						value={password}
+						onChange={changePassword}
+						required
+					/>
+					<button className="btn" type="submit" onClick={authenticate}>
 						Log In
 					</button>
 				</form>
+				<p class="signup-link">
+					Don't have an account? <Link to="/signup">Register</Link>
+				</p>
+				<ul className="error-text">
+					{errorMessage !== "" && <li>{errorMessage}</li>}
+				</ul>
 			</div>
 		</div>
 	);
