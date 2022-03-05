19f7869e2d4fcde98cddc6a3590b2bed20ecb16e
Saurav Sah <sah_64@yahoo.com>
2022-03-05 13:20:12 -0500

    uninstalled sessions from server dependencies

--- server/app.js
+++ server/app.js
@@ -3,7 +3,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const morgan = require("morgan");
-const session = require("express-session");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const passport = require("passport");
